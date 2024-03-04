23eff814a6e0160a6835f74a5de5965e2b9a982e
SwapnilB03 <swapnilbhoyarr827@gmail.com>
2024-03-04 20:31:26 +0530

    push gym exercise

---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState }from 'react';
+//it is a hook
+import { useParams } from 'react-router-dom';
+// We are going to use this to determine the id of the exerecise we are currently on so we can fetch additional data about that exercise
+import { Box } from '@mui/material';
+
+import { exerciseOptions, fetchData, youtubeOptions } from '../Utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExercisesVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
+const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    //It is going to have a dependency array that is going to have the id inside of it meaning we are going to recall this function
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+// This is youtube search and download API
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
+      setExerciseVideos(exerciseVideosData.contents);
+
+      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equimentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      setEquipmentExercises(equimentExercisesData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  if (!exerciseDetail) return <div>No Data</div>;
+
+  return (
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
